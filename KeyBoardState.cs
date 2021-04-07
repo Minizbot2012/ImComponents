e92b450908960a8f007fc2f219024a64b591cf0f
Minizbot2012 <mini@zbots.org>
2021-04-07 13:05:40 -0400

    Initial Commit

---
+++ KeyBoardState.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+
+namespace ImGuiComponents
+{
+    public static class Keyboard
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+        public static byte[] GetKeyboard()
+        {
+            var array = new byte[256];
+            GetKeyboardState(array);
+            return array;
+        }
+        public static bool IsPressed(byte ch)
+        {
+            return (ch & 0x80) != 0;
+        }
+    }
+}
